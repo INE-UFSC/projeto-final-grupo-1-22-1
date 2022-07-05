2ede6a81bf4fa84277296b9fc1c06a859eb2442d
eduperottoni <edu.perottoni@gmail.com>
2022-07-05 00:48:43 -0300

    Iniciando controlador de telas

--- versao_final/Models/ControladorTelas.py
+++ versao_final/Models/ControladorTelas.py
@@ -1,2 +1,6 @@
+from Models.Tela import Tela
+
 class ControladorTelas:
-  def __init__()
+    def __init__(self, tela_menu: Tela, tela_creditos): #tela_game_over, tela_pause
+        self.tela_menu = tela_menu
+        self.tela_creditos = tela_creditos
