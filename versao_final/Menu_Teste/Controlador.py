9ac0352d98818ae05abf7ad62930208cb7586dc1
Lucas Tinoco <lucasltinoco@gmail.com>
2022-07-10 21:32:14 -0300

    implementa prova de conceito do padrão state

--- versao_final/Menu_Teste/Controlador.py
+++ versao_final/Menu_Teste/Controlador.py
@@ -1,71 +1,66 @@
-from http.client import CONFLICT
-from pathlib import WindowsPath
 import pygame as pg
 from States.State import Context
 from States.MenuPrincipal import MenuPrincipal
-import button
+from States.Jogo import Jogo
+from States.Creditos import Creditos
+from States.Options import Options
+from States.Ranking import Ranking
+
+STATES = {
+    "main": MenuPrincipal,
+    "jogo": Jogo,
+    "credits": Creditos,
+    "options": Options,
+    "ranking": Ranking
+}
+
 
 class Controlador():
-  def __init__(self):
-    pg.init()
+    def __init__(self):
+        pg.init()
 
-    SCREEN_WIDTH = 640
-    SCREEN_HEIGHT = 480
+        SCREEN_WIDTH = 640
+        SCREEN_HEIGHT = 480
 
-    self.__window = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    pg.display.set_caption("Copper Temple - Menus")
+        self.__window = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pg.display.set_caption("Copper Temple - Menus")
 
+        # self.__game_paused = True
+        self.__state = "main"
 
-    self.__game_paused = True
-    self.__menu_state = "main"
+        icon_img = pg.image.load("images/Icon.png")
 
-    self.__font = pg.font.SysFont("arialblack", 40)
+        pg.display.set_icon(icon_img)
 
-    self.__TEXT_COLLOR = (255, 255, 255)
+    @property
+    def state(self):
+        return self.__state
 
+    @state.setter
+    def state(self, state) -> None:
+        self.__state = state
 
-    icon_img = pg.image.load("images/Icon.png")
+    def run(self):
+        run = True
 
+        tela = Context(MenuPrincipal(self.__window, self))
 
+        while run:
+            if self.__state in STATES:
+                tela.renderizar(STATES[self.__state])
 
+            # else:
+            #   self.draw_text("Press SPACE to pause", self.__font, self.__TEXT_COLLOR, 160, 250)
 
-    pg.display.set_icon(icon_img)
+            for event in pg.event.get():
+                if event.type == pg.KEYDOWN:
+                    # if event.key == pg.K_SPACE:
+                    #     self.__game_paused = True
+                    if event.key == pg.K_ESCAPE:
+                        self.__state = "main"
+                if event.type == pg.QUIT:
+                    run = False
 
-  def draw_text(self, text, font, text_col, x, y):
-      img = font.render(text, True, text_col)
-      self.__window.blit(img, (x, y))
+            pg.display.update()
 
-  def run(self):
-    run = True
-
-    while run:
-        tela = Context(MenuPrincipal(self.__window))
-       # if self.__game_paused == True:
-
-        # if self.__menu_state == "ranking":
-        #     self.__window.fill((52, 78, 91))
-        #     self.draw_text("Ranking menu", self.__font, self.__TEXT_COLLOR, 160, 250)
-
-        # if self.__menu_state == "options":
-        #     self.__window.fill((52, 78, 91))
-        #     self.draw_text("Options menu", self.__font, self.__TEXT_COLLOR, 160, 250)
-
-        # if self.__menu_state == "credits":
-        #     self.__window.fill((52, 78, 91))
-        #     self.draw_text("Credits menu", self.__font, self.__TEXT_COLLOR, 160, 250)
-
-        #else:
-         #   self.draw_text("Press SPACE to pause", self.__font, self.__TEXT_COLLOR, 160, 250)
-
-        for event in pg.event.get():
-            if event.type ==pg.KEYDOWN:
-                if event.key == pg.K_SPACE:
-                    self.__game_paused = True
-                if event.key == pg.K_ESCAPE:
-                    self.__menu_state = "main"
-            if event.type == pg.QUIT:
-                run = False
-        
-        pg.display.update()
-
-    pg.quit()
+        pg.quit()
--- versao_final/Menu_Teste/button.py
+++ versao_final/Menu_Teste/button.py
@@ -1,4 +1,3 @@
-from turtle import width
 import pygame as pg
 
 class Button():
